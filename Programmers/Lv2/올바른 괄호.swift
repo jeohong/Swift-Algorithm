f2e7d03c645e8f06742a441b6ce7954fbe5a8ac8
jeohong <jeohong59@gmail.com>
2023-09-14 15:33:09 +0900

    레벨2 - 구현( 효율성 개선 )

--- Programmers/Lv2/올바른 괄호.swift
+++ Programmers/Lv2/올바른 괄호.swift
@@ -1,16 +1,37 @@
+//import Foundation
+
+//func solution(_ s:String) -> Bool
+//{
+//    var resultStack = [Character]()
+//    
+//    s.forEach { item in
+//               if !resultStack.isEmpty && item == ")" && resultStack.last! == "(" {
+//                   resultStack.removeLast()
+//               } else {
+//                   resultStack.append(item)
+//               }
+//              }
+//    return resultStack.isEmpty
+//}
+
+// 효율성 개선
 import Foundation
 
 func solution(_ s:String) -> Bool
 {
-    var resultStack = [Character]()
+    var count = 0
+    var result = true
     
-    s.forEach { item in
-               if !resultStack.isEmpty && item == ")" && resultStack.last! == "(" {
-                   resultStack.removeLast()
-               } else {
-                   resultStack.append(item)
-               }
-              }
-    return resultStack.isEmpty
+    for item in s {
+        if item == "(" {
+            count += 1
+        } else if item == ")" && count <= 0 {
+            result = false
+            break
+        } else {
+            count -= 1
+        }
+    }
+    
+    return count == 0 && result
 }
-
